a030fde325587d7baa8911317f8ae550a2e4eb20
Richard Pianka <richardpianka@gmail.com>
2012-05-15 22:15:13 -0400

    Nothing visible in the diff...committing anyways.

--- src/Chess/Game/Player.scala
+++ src/Chess/Game/Player.scala
@@ -4,7 +4,7 @@ abstract class Player(val board: Board, val color: Color) {
   val allPieces = board.allPieces.filterNot(_._2.isEmpty)
                                  .filter(_._2.get.color == color)
                                  .toSeq
-  
+
   val movablePieces = allPieces.filterNot(x => x._2.get.getMoves(x._1, board).isEmpty).toSeq
 
   def getNextMove: Move
