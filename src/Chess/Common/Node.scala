fb66ce670081e4dcdeaa00e2bcc1f6c4f84c3c08
Richard Pianka <richardpianka@gmail.com>
2012-05-21 13:21:59 -0400

    Created a DAG node and the start of a minimax implementation.  Going to go upstairs and continue work now.

---
+++ src/Chess/Common/Node.scala
@@ -0,0 +1,17 @@
+package Chess.Common
+
+/**
+ * Directed acyclic graph
+ *
+ * @param value The payload for this node
+ * @param children The children (or none) of this node
+ * @tparam A The common type of the graph's payload
+ */
+class Node[A](val value: A, val children: Option[Seq[Node[A]]] = None) {
+  /**
+   * Whether or not this node is at the end of the tree
+   *
+   * @return Whether or not this node is at the end of the tree
+   */
+  def isLeaf = children.isEmpty
+}
--- src/Chess/Players/MinimaxPlayer.scala
+++ src/Chess/Players/MinimaxPlayer.scala
@@ -3,15 +3,13 @@ package Chess.Players
 import Chess.Game._
 import util.Random
 
-/**
- * Picks a random possible move, and makes it
- */
-class RandomPlayer extends PlayerFactory {
+class MinimaxPlayer extends PlayerFactory {
   def get(board: Board, color: Color) = new Player(board, color) {
     def nextMove: Move = {
-      val piecesThatCanMove = board.playablePieces.filter(_._2.get.color == color) // where that piece is our color
-                                             .filterNot(x => x._2.get.getMoves(x._1, board).isEmpty) // which can move
-                                             .toSeq
+      val piecesThatCanMove = board.playablePieces.filterNot(_._2.isEmpty) // squares where there is a piece
+        .filter(_._2.get.color == color) // where that piece is our color
+        .filterNot(x => x._2.get.getMoves(x._1, board).isEmpty) // which can move
+        .toSeq
       val random = new Random()
       val piece = piecesThatCanMove(random.nextInt(piecesThatCanMove.size - 1))
       val allMoves = piece._2.get.getMoves(piece._1, board)
