5b51a489848fd768ed355310bfeb19b6b4282e8f
pianka <richardpianka@gmail.com>
2012-06-25 23:12:22 -0400

    Trying to fix this repo.

---
+++ src/com/richardpianka/Chess/Server/accounts/Account.scala
@@ -0,0 +1,13 @@
+package com.richardpianka.chess.server.accounts
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pianka
+ * Date: 6/25/12
+ * Time: 10:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Account {
+
+}
